ae3d06ee2e5e54bdfc26247f24cad87864eb46c5
Reginald Chand <reginald-chand@outlook.com>
2024-06-24 17:58:45 +1200

    Create post.create.controller.validator.mjs

---
+++ server/validators/post/post.create.controller.validator.mjs
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export const postCreateControllerValidator = Joi.object({
+  _csrf: Joi.string().required(),
+
+  postTitle: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+  postContent: Joi.string().required(),
+  postAuthor: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+
+  serverResponse: Joi.object().required(),
+});
